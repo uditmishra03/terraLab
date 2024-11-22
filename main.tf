e2879016a57f85b7b9183b483e3d60d89aa8c3a3
uditmishra03 <uditmishra.um@gmail.com>
2024-11-22 19:24:36 +0530

    First terraform Configuration file

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "ap-south-1"  # Set your desired AWS regio
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0dee22c13ea7a9a67"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
